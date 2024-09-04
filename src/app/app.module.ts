2b5e0cdc09cb65376533d22b46e24175ee8967ff
mezrioui hakim <mezrioui.hakim@gmail.com>
2024-09-04 21:21:26 +0100

    hero section complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { IconsLinkComponent } from './components/icons-link/icons-link.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HeroSectionComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HeroSectionComponent,
+    IconsLinkComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/icons-link/icons-link.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icons-link',
+  templateUrl: './icons-link.component.html',
+  styleUrls: ['./icons-link.component.css']
+})
+export class IconsLinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
