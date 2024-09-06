6844139d7ae7fc1ef5724efa2cdd46c5af49508c
Hakim2Mezrioui <mezrioui.hakim@gmail.com>
2024-09-06 07:40:16 +0100

    last changes of skills

--- src/app/components/skills-section/skills-section.component.ts
+++ src/app/components/skills-section/skills-section.component.ts
@@ -13,12 +13,29 @@ import {
 export class SkillsSectionComponent implements AfterViewInit {
   @ViewChild('skills') skills!: ElementRef;
 
-  constructor() {}
+  constructor() {
+
+  }
+
+   skillsLanguage: string[] = [
+    "https://miro.medium.com/v2/resize:fit:900/1*TY9uBBO9leUbRtlXmQBiug.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png",
+    "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
+    "https://i1.wp.com/www.keesmel.com/wp-content/uploads/2020/09/js-logo.png?fit=500%2C500",
+    "https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg"
+  ];
+  skillsFramework: string[] = [
+    "https://miro.medium.com/v2/resize:fit:900/1*TY9uBBO9leUbRtlXmQBiug.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png",
+    "https://cdn-icons-png.flaticon.com/512/5968/5968242.png",
+    "https://i1.wp.com/www.keesmel.com/wp-content/uploads/2020/09/js-logo.png?fit=500%2C500",
+    "https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg"
+  ];
 
   ngAfterViewInit(): void {
     // Récupère les enfants du carrousel une fois le composant initialisé
     const skills = this.skills.nativeElement.children as HTMLCollectionOf<HTMLElement>;
-    
+
     // Appelle la fonction de carrousel chaque 2 secondes
     setInterval(() => {
       this.carousel(skills);
