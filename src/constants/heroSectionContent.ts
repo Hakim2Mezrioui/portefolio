e4ec016eaed61745bf961d9f6a400c24ff920c51
Hakim Mezrioui <mezrioui.hakim@gmail.com>
2026-02-22 17:07:36 +0000

    Refactor hero section styles and content, enhance accessibility with updated link labels, and improve icons layout with tooltips. Updated hero section description for clarity and impact.

--- src/constants/heroSectionContent.ts
+++ src/constants/heroSectionContent.ts
@@ -1,8 +1,21 @@
 import { HeroSectionModel } from 'src/app/models/heroSectionModel';
 export const heroSectionContent = new HeroSectionModel(
   'WELCOME.',
-  "I'AM MEZRIOUI HAKIM",
+  "I AM HAKIM MEZRIOUI",
   `
-    I am a web and mobile developer with a solid background in both Electromechanics and Digital IT. I hold two diplomas—one in Electromechanics and the other in Digital IT—and I continuously expand my skill set by learning new technologies, including the Internet of Things (IoT). My experience spans a wide range of projects, from mobile apps to web platforms, with a focus on creating innovative solutions and efficient systems. Passionate about technology and problem-solving, I am eager to apply my expertise in dynamic environments to drive impactful digital transformation.
-`
+  I am a Full Stack Software Engineer and IoT Specialist with a unique dual background in Digital IT and Electromechanics. 
+  Currently driving digital transformation initiatives, I specialize in building comprehensive systems—from secure, 
+  scalable web platforms to smart hardware integrations.
+
+  My diverse expertise allows me to seamlessly connect the physical and digital worlds. 
+  Whether I am developing a facial recognition attendance system or architecting an AI-driven mobile application, 
+  I am deeply passionate about solving complex technical challenges and delivering impactful, user-centric technologies.
+  `
+  // `
+  //   I am a web and mobile developer with a solid background in both Electromechanics and Digital IT. 
+  //   I hold two diplomas—one in Electromechanics and the other in Digital IT—and I continuously expand my skill set by learning new technologies, 
+  //   including the Internet of Things (IoT). My experience spans a wide range of projects, from mobile apps to web platforms, 
+  //   with a focus on creating innovative solutions and efficient systems. Passionate about technology and problem-solving, 
+  //   I am eager to apply my expertise in dynamic environments to drive impactful digital transformation.
+  // `
 );
